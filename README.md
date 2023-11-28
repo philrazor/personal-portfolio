0e481a9064264e14ac6446fa4b1096e2f9a33f26
Philip Njoroge <114145385+philrazor@users.noreply.github.com>
2023-11-28 20:35:26 +0300

    finished with the building

---
+++ README.md
@@ -0,0 +1,2 @@
+# portfolio-project-complete
+Just regurlar update needed
---
+++ script.js
@@ -0,0 +1,6 @@
+function toggle_menu() {
+    const menu = document.querySelector(".menu-links") ; 
+    const icon = document.querySelector(".humburger-icon") ;
+    menu.classList.toggle("open") ;
+    icon.classList.toggle("open") ; 
+}
